3a0a2084e89fd36374558aa810a8de67432aadd7
Note do Dan <danielmaiareis@outlook.com>
2024-07-11 17:13:44 -0300

    criado novo schema

--- fast_training/app.py
+++ fast_training/app.py
@@ -1,9 +1,13 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_training.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'hello world'}
 
